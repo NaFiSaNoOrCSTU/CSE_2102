9c0c5a83867b8cc29d02c66c49eadc4f6e4ff10c
Nafisa_Noor <nafisa.noor.1609@gmail.com>
2025-05-18 18:20:38 +0600

    Add files via upload

---
+++ insert_node_in_location.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_pos(Node *head,int pos, int item)
+{
+    Node *newNode = new Node(item);
+    Node* tmp=head;
+    for(int i=1;i<pos-1;++i)
+    {
+        tmp=tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+}
+void print(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    Node *head = new Node(20);
+    Node *a = new Node(30);
+    Node *b = new Node(40);
+    Node *c = new Node(50);
+    Node *d = new Node(60);
+    head->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    cout << "item you want to insert: " << endl;
+    int item;
+    cin >> item;
+    int pos;
+    cout << "position where you want to insert: " << endl;
+    cin >> pos;
+    insert_pos(head, pos,item);
+    print(head);
+    return 0;
+}
